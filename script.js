6eae065a66d80f08c3b7aa7b7547b47a23f2b03d
Rodrigo Tufiño <rtufino@ups.edu.ec>
2025-06-09 16:42:41 -0500

    variables y consola

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 function mostrarMensaje(){
     alert("Hola mundo :)");
 }
+
+// Variables
+function imprimirVariables(){
+    let nombre = "Rodrigo";
+    let edad = 42;
+    // Forma tradicional de concatenar cadenas
+    console.log("nombre: ", nombre);
+    console.log("edad: ", edad);
+    // Forma moderna de concatenar cadenas
+    console.log(`nombre: ${nombre}, edad: ${edad}`);
+    // Otros tipos de impresión
+    console.error("Error: bla bla bla");
+    console.warn("Advertencia: x x x");
+}
