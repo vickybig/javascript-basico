f1f92a5e096ec8a5880a25da5ed43b9fc36f5eae
Victor Aimacaña <vaimacana@est.ups.edu.ec>
2025-06-16 15:50:46 -0500

    Validacion datos

--- script.js
+++ script.js
@@ -31,3 +31,55 @@ function arreglos(){
         console.log('No es una pera');
     }
 }
+// Agregar evente a un elemento HTML
+document.getElementById("btnSaludar").addEventListener(
+    "click", function () {
+        console.log("Hola!");
+    }
+);
+
+// Modificar el HTML a través del DOM
+document.getElementById("btnAgregar").addEventListener(
+    "click", () => {
+        // Obtener el elemnto "lista" (ul) del documento HTML
+        let lista = document.getElementById("lista");
+        // Creando un nuevo elemento HTML, li = list item
+        let nuevoItem = document.createElement("li");
+        // Obteniendo el total de ítems de la lista
+        let totalItems = lista.childElementCount;
+        // Agregando un contenido (texto) al item
+        nuevoItem.textContent = `Item ${totalItems + 1}`;
+        // Agregar item a la lista
+        lista.appendChild(nuevoItem);
+    }
+)
+
+function addPalabraLista(){
+    console.log ("funciomo");
+
+}
+document.getElementById("btnAddPalabra").addEventListener(
+    "click", addPalabraLista
+
+);
+
+function addPalabraLista(){
+    // Obtener el input del HTML
+    let input = document.getElementById("txtEntrada");
+    // Obtner el valor del input
+    const texto = input.value.trim();
+    if (texto === "") return;
+    // Transformar a mayusculas
+    const textMayuscula = texto.toUpperCase();
+    // Agregar item a la lista
+    let lista = document.getElementById("lstPalabras");
+    // Creando un nuevo elemento HTML, li = list item
+    let nuevoItem = document.createElement("li");
+    // Agregando un contenido (texto) al item
+    nuevoItem.textContent = textMayuscula;
+    // Agregar item a la lista
+    lista.appendChild(nuevoItem);
+    // limpiar y establecer foco
+    input.value = "";
+    input.focus();
+}
